03bed710cdab0752426b1e10012d8cc6c0bdaa10
pmutua <pmutua@live.com>
2017-03-07 16:34:10 +0300

    update html javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 //login js
 $(document).ready(function () {
-  $("#landing").slideDown(4000);
-  var Anita = "A3220";
-  $("form#blanks").submit(function (event) {
-    event.preventDefault();
-    var userInput = $("input#foodID").val();
-    $("input#foodID").val("");
-    if (userInput === Anita) {
-      alert("Welcome to Anita's Kitchen");
-    }
-    else {
-      alert("Sorry wrong ID try again");
-    };
-  });
+$("#landing").slideDown(4000);
+var Anita = "A3220";
+$("form#blanks").submit(function (event) {
+event.preventDefault();
+var userInput = $("input#foodID").val();
+$("input#foodID").val("");
+if (userInput === Anita) {
+alert("Welcome to Anita's Kitchen");
+}
+else {
+alert("Sorry wrong ID try again");
+};
 });
+});
+//end
+
+//feedback form
+$(document).ready(function(){
+jQuery(".pull_feedback").toggle(function(){
+jQuery("#feedback").animate({left:"0px"});
+return false;
+},
+function(){
+jQuery("#feedback").animate({left:"-362px"});
+return false;
+}
+); //toggle
+}); //document.ready
+
+//end
+
