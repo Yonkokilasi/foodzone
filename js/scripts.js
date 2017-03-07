6eb9aeb8f9bba66a3c5813aef489b2c842d0ac77
Yonkokilasi <yonkokilasi@gmail.com>
2017-03-07 23:02:22 +0300

    update2

--- js/scripts.js
+++ js/scripts.js
@@ -10,32 +10,31 @@ function closeNav() {
 }
 //end 
 //login js
-$(document).ready(function() {
-    $("#landing").slideDown(4000);
-    var Anita = "A3220";
-    $("form#blanks").submit(function(event) {
-        event.preventDefault();
-        var userInput = $("input#foodID").val();
-        $("input#foodID").val("");
-        if (userInput === Anita) {
-            alert("Welcome to Anita's Kitchen");
-        } else {
-            alert("Sorry wrong ID try again");
-        };
+$(document).ready(function () {
+  $("#landing").slideDown(4000);
+  var Anita = "A3220";
+  $("form#blanks").submit(function (event) {
+    event.preventDefault();
+    var userInput = $("input#foodID").val();
+    $("input#foodID").val("");
+    if (userInput === Anita) {
+      alert("Welcome to Anita's Kitchen");
+    }
+    else {
+      alert("Sorry wrong ID try again");
+    };
+  });
+  jQuery(".pull_feedback").toggle(function () {
+    jQuery("#feedback").animate({
+      left: "0px"
     });
+    return false;
+  }, function () {
+    jQuery("#feedback").animate({
+      left: "-362px"
+    });
+    return false;
+  }); //toggle
 });
 //end
-
-        //Feedback html
-
-$(document).ready(function(){
-    jQuery(".pull_feedback").toggle(function(){
-        jQuery("#feedback").animate({left:"0px"});
-        return false;
-      },
-      function(){
-        jQuery("#feedback").animate({left:"-362px"}); 
-        return false;
-      }
-    ); //toggle
-  }
+//Feedback html
