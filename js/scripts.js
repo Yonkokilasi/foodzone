70a0fc29d9e524e9c17a37352509ad3c6932e38f
collins <collins@localhost.localdomain>
2017-03-09 19:24:35 +0300

    updates

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//nav bar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0%";
+
+}
+//end
+//login js
+$(document).ready(function () {
+    $("#landing").slideDown(4000);
+    var Anita = "A3220";
+    $("form#blanks").submit(function (event) {
+        event.preventDefault();
+        var userInput = $("input#foodID").val();
+        $("input#foodID").val("");
+        if (userInput === Anita) {
+            alert("Welcome to Anita's Kitchen");
+        } else {
+            alert("Sorry wrong ID try again");
+        };
+    });
+
+});
+//end
+//Feedback html
